5a1b86c1fcf71aeac97a9993cf8c1d5d40703f35
denis1222223 <denis1222223@mail.ru>
2016-09-26 01:20:47 +0300

    project 7 completed

---
+++ WT_Lab1_Project7/src/Project7/MainTest.java
@@ -0,0 +1,17 @@
+package Project7;
+
+import junit.framework.TestCase;
+import static org.junit.Assert.*;
+import static Project7.Main.shellSort;
+
+
+public class MainTest extends TestCase {
+
+    public void testShellSort() throws Exception {
+        double[] numbers = new double[]{ 3,2,4,-1 };
+        numbers = shellSort(numbers);
+        double test = 0;
+        assertArrayEquals(new double[]{-1,2,3,4}, numbers, test);
+    }
+
+}

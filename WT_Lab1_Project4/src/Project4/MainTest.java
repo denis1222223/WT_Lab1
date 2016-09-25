84e0f0dcc8c2fe38c37fc532265d4eeb6eb3f818
denis1222223 <denis1222223@mail.ru>
2016-09-25 17:02:54 +0300

    p4 completed

--- WT_Lab1_Project4/src/Project4/MainTest.java
+++ WT_Lab1_Project4/src/Project4/MainTest.java
@@ -2,7 +2,27 @@ package Project4;
 
 import junit.framework.TestCase;
 
+import static Project4.Main.findPrimeNumbers;
+import static Project4.Main.isPrimeNumber;
+import static org.junit.Assert.*;
+
 public class MainTest extends TestCase {
 
+    public void testIsPrimeNumber() throws Exception {
+        int number = 13;
+        boolean prime = isPrimeNumber(number);
+        assertEquals(true, prime);
+    }
 
+    public void testIsNotPrimeNumber() throws Exception {
+        int number = 44;
+        boolean prime = isPrimeNumber(number);
+        assertEquals(false, prime);
+    }
+
+    public void testFindPrimeNumbers() throws Exception {
+        int[] numbers = new int[]{ 1, 3, 4 , 7, 6 };
+        int[] primeNumbers = findPrimeNumbers(numbers);
+        assertArrayEquals(new int[]{0,1,3}, primeNumbers);
+    }
 }
